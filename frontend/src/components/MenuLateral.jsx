464b969c169f6a3584d7eb1140f89cfe9f3f3760
Kevin-AC <kevin.argumedo@gmail.com>
2026-03-20 23:35:20 -0500

    Feat: Componente Menu y iconos relacionados

---
+++ frontend/src/components/MenuLateral.jsx
@@ -0,0 +1,47 @@
+export default function MenuLateral() {
+    return (
+        <section className="w-75 py-12 px-2 h-screen shadow-lg bg-MainBackground flex flex-col justify-between">
+                <div>
+                <div className="flex gap-2">
+                    <img className="w-12" src="src/assets/Logo.svg" alt="Logo" />
+                    <article>
+                        <h1 className="text-3xl text-BlueHover font-medium">Autos Colombia</h1>
+                        <p className="text-Paragraf font-medium">Parking Management</p>
+                    </article>
+                </div>
+
+                <nav class="w-full mt-12">
+                    <ul class="py-6 space-y-2 font-medium">
+                        <li><a href="#" class="flex items-center gap-2 px-6 py-4 group hover:scale-[1.02] hover:-translate-x-1 transition-all duration-300 ease-out overflow-hidden  text-Paragraf hover:bg-BlueHover/10 hover:text-BlueHover rounded-r-lg">
+                            <img src="src/assets/Container.svg" alt="icon Dashboard" />
+                            <span>Dashboard</span></a>
+                        </li>
+                        <li><a href="#" class="flex items-center gap-2 px-6 py-4 group hover:scale-[1.02] hover:-translate-x-1 transition-all duration-300 ease-out overflow-hidden  text-Paragraf hover:bg-BlueHover/10 hover:text-BlueHover rounded-r-lg">
+                            <img src="src/assets/Users.svg" alt="user icon" />
+                            <span>Users</span>
+                        </a></li>
+                        <li><a href="#" class="flex items-center gap-2 px-6 py-4 group hover:scale-[1.02] hover:-translate-x-1 transition-all duration-300 ease-out overflow-hidden  text-Paragraf hover:bg-BlueHover/10 hover:text-BlueHover rounded-r-lg">
+                            <img src="src/assets/Vehicles.svg" alt="user icon" />
+                            <span>Vehicles</span>
+                        </a></li>
+                        <li><a href="#" class="flex items-center gap-2 px-6 py-4 group hover:scale-[1.02] hover:-translate-x-1 transition-all duration-300 ease-out overflow-hidden  text-Paragraf hover:bg-BlueHover/10 hover:text-BlueHover rounded-r-lg">
+                            <img src="src/assets/Payments.svg" alt="user icon" />
+                            <span>Payments</span>
+                        </a></li>
+                        <li><a href="#" class="flex items-center gap-2 px-6 py-4 group hover:scale-[1.02] hover:-translate-x-1 transition-all duration-300 ease-out overflow-hidden  text-Paragraf hover:bg-BlueHover/10 hover:text-BlueHover rounded-r-lg">
+                            <img src="src/assets/Reports.svg" alt="user icon" />
+                            <span>Reports</span>
+                        </a></li>
+                    </ul>
+                </nav>
+                </div>
+ 
+            <div className="flex gap-2 px-6 group hover:scale-[1.02] hover:-translate-x-1 transition-all duration-300 ease-out overflow-hidden">
+                        <img src="src/assets/Logout.svg" alt="logout icons" />
+                        <p className="text-Paragraf font-medium hover:text-BlueHover">Logout</p>
+                </div>
+           
+
+        </section>
+    )
+}
