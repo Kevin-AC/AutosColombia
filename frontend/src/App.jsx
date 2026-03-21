27d291babc106ef37455856f54191b0a7530e36a
Kevin-AC <kevin.argumedo@gmail.com>
2026-03-20 23:36:04 -0500

    Chore: variables de colores, cambio de titulo de pagina

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
+import MenuLateral from "./components/MenuLateral"
 
 function App() {
  
   return (
     <>
-    <h1 className="text-7xl font-bold text-red-600">Hola mundo</h1>
+      <MenuLateral/>
       
     </>
   )
