9d098d23c5e73bde6da14b05ebfb939070895d5e
Kevin-AC <kevin.argumedo@gmail.com>
2026-03-22 22:27:07 -0500

    Chore:Ruta para GestionVehiculos

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,14 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import GestionUsuarios from './pages/GestionUsuarios.jsx'
+import GestionVehiculos from './pages/GestionVehiculos.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/Usuarios" element={<GestionUsuarios />} />
+      <Route path="/Vehiculos" element={<GestionVehiculos />} />
     </Routes>
   </BrowserRouter>
 )
