b319ab8e75b593eb0603863a66d3c994f0ee78a9
Carolina-H-8 <astrid.hernandez@est.iudigital.edu.co>
2026-03-23 17:56:14 -0500

    Correcion

--- backend/src/main/java/com/autoscolombia/backend/Controller/UsuarioController.java
+++ backend/src/main/java/com/autoscolombia/backend/Controller/UsuarioController.java
@@ -16,20 +16,20 @@ public class UsuarioController {
     @Autowired
     private UsuarioDao usuarioRepo;
 
-    // 1. CREATE: Guardar un usuario nuevo (RF-04)
+
     @PostMapping
     public ResponseEntity<Usuario> guardarUsuario(@RequestBody Usuario usuario) {
         Usuario nuevoUsuario = usuarioRepo.save(usuario);
         return ResponseEntity.ok(nuevoUsuario);
     }
 
-    // 2. READ: Obtener la lista de todos los usuarios
+
     @GetMapping
     public List<Usuario> listarTodos() {
         return usuarioRepo.findAll();
     }
 
-    // 3. READ: Buscar un usuario específico por su cédula
+
     @GetMapping("/{cedula}")
     public ResponseEntity<Usuario> obtenerPorCedula(@PathVariable String cedula) {
         return usuarioRepo.findById(cedula)
@@ -37,7 +37,7 @@ public class UsuarioController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    // 4. UPDATE: Actualizar datos de un usuario
+
     @PutMapping("/{cedula}")
     public ResponseEntity<Usuario> actualizar(@PathVariable String cedula, @RequestBody Usuario datosNuevos) {
         return usuarioRepo.findById(cedula).map(usuario -> {
@@ -47,7 +47,7 @@ public class UsuarioController {
         }).orElse(ResponseEntity.notFound().build());
     }
 
-    // 5. DELETE: Eliminar un usuario
+
     @DeleteMapping("/{cedula}")
     public ResponseEntity<Void> eliminar(@PathVariable String cedula) {
         if (usuarioRepo.existsById(cedula)) {
